a16bee0162840528b7978fe890d57e635c88038e
n <alanfalcon122@gmail.com>
2024-05-06 17:46:18 -0300

    Intercambio inicial de proyecto

---
+++ Catalogo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.ArrayList;
+
+/**
+ * Write a description of class Catalogo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Catalogo
+{ 
+    public ArrayList<Book> Catalogo; 
+    public Catalogo()
+     
+    
+    
+    
+    
+    }
